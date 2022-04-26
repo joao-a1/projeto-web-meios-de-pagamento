81afdcffcafd8dad3f06a4e627d274b2f000798c
joao.v121@aluno.ifsc.edu.br <IFSC@CDR-718113.cacador.ifsc.edu.br>
2022-04-25 21:37:09 -0300

    Criação Model e Migration de Empresas parceiras

---
+++ database/migrations/2022_04_26_002600_create_empresas.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('razaoSocial',100);
+            $table->integer('CNPJ');
+            $table->string('telefone',15);
+            $table->string('tokenAcesso',100);
+            $table->double('saldo', 15, 2);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empresas');
+    }
+};
