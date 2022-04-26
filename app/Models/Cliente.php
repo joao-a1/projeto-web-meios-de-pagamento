8e10c0ba15ebbaf9696371d939eca6e8b7be6a1e
Aluno IFSC <IFSC@CDR-718116.cacador.ifsc.edu.br>
2022-04-25 22:06:35 -0300

    Alteração joao_a

---
+++ app/Models/Cliente.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Models\Cidade;
+
+class Cliente extends Model
+{
+    use HasFactory;
+    protected $table = "clientes";
+
+    function cidades(){
+        return $this->belongsTo(Cidade::class, 'id_cidade', 'id');
+    }
+}
