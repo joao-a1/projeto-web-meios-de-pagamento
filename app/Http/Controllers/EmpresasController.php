5c64fc7f004ecfa94d989f71598c59272eb991ce
Joao Arthur <joao.a1@aluno.ifsc.edu.br>
2022-05-14 17:36:30 -0300

    Empresas, Transacoes, Status Controllers

---
+++ app/Http/Controllers/EmpresasController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Empresa;
+
+class EmpresasController extends Controller
+{
+    function cadastro_novo(){
+        return view('nova_empresa');
+    }
+
+    function novo(Request $req){
+        $razao_social = $req->input('razao_social');
+        $cnpj = $req->input('cnpj');
+        $telefone = $req->input('telefone');
+        $email = $req->input('email');
+        $token_acesso = $req->input('token_acesso');
+        $saldo = $req->input('saldo');
+
+        $empresa = new Empresa();
+        $empresa->razao_social = $razao_social;
+        $empresa->cnpj = $cnpj;
+        $empresa->telefone = $telefone;
+        $empresa->email = $email;
+        $empresa->token_acesso = $token_acesso;
+        $empresa->saldo = $saldo;
+
+        $empresa->save();
+
+        return redirect()->route('empresas_listar');
+    }
+
+    function listar(){
+        $empresas = Empresa::all();
+
+        return view('lista_empresa', ['empresas' => $empresas]);
+    }
+
+    function alterar($id){
+        $empresa = Empresa::findOrFail($id);
+
+        return view('altera_empresa', ['empresas' => $empresa]);
+    }
+
+    function salvar(Request $req){
+        $id = $req->input('id');
+        $razao_social = $req->input('razao_social');
+        $cnpj = $req->input('cnpj');
+        $telefone = $req->input('telefone');
+        $email = $req->input('email');
+        $token_acesso = $req->input('token_acesso');
+        $saldo = $req->input('saldo');
+
+        $empresa = Empresa::findOrFail($id);
+        $empresa->razao_social = $razao_social;
+        $empresa->cnpj = $cnpj;
+        $empresa->telefone = $telefone;
+        $empresa->email = $email;
+        $empresa->token_acesso = $token_acesso;
+        $empresa->saldo = $saldo;
+
+        $empresa->save();
+
+        return redirect()->route('empresas_listar');
+    }
+
+    function excluir($id){
+        $empresa = Empresa::findOrFail($id);
+        $empresa->delete();
+
+        return redirect()->route('empresas_listar');
+    }
+}
---
+++ app/Http/Controllers/StatusController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Status;
+
+class StatusController extends Controller
+{
+    function cadastro_novo(){
+        return view('novo_status');
+    }
+
+    function novo(Request $req){
+        $nome_status = $req->input('nome_status');
+
+        $status = new Status();
+        $cliente->nome_status = $nome_status;
+
+        $status->save();
+
+        return redirect()->route('status_listar');
+    }
+
+    function listar(){
+        $status = Status::all();
+
+        return view('lista_status', ['status' => $status]);
+    }
+
+    function alterar($id){
+        $status = Status::findOrFail($id);
+
+        return view('altera_status', ['status' => $status]);
+    }
+
+    function salvar(Request $req){
+        $id = $req->input('id');
+        $nome_status = $req->input('nome_status');
+
+        $status = Status::findOrFail($id);
+        $status->nome_status = $nome_status;
+
+        $status->save();
+
+        return redirect()->route('status_listar');
+    }
+
+    function excluir($id){
+        $status = Status::findOrFail($id);
+        $status->delete();
+
+        return redirect()->route('status_listar');
+    }
+}
