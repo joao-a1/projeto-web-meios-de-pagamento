bc004359ba96bb422ba316677b1becd403ba50ab
Joao Arthur <joao.a1@aluno.ifsc.edu.br>
2022-05-14 15:46:02 -0300

    Clientes, Cidades, Creditos Controller

---
+++ app/Http/Controllers/CidadesController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Cidade;
+
+class CidadesController extends Controller
+{
+    function cadastro_novo(){
+        return view('nova_cidade');
+    }
+
+    function novo(Request $req){
+        $nome = $req->input('nome');
+        $estado = $req->input('estado');
+
+        $cidade = new Cidade();
+        $cidade->nome = $nome;
+        $cidade->estado = $estado;
+
+        $cidade->save();
+
+        return redirect()->route('cidades_listar');
+    }
+
+    function listar(){
+        $cidades = Cidade::all();
+
+        return view('lista_cidade', ['cidades' => $cidades]);
+    }
+
+    function alterar($id){
+        $cidade = Cidade::findOrFail($id);
+
+        return view('altera_cidade', ['cidade' => $cidade]);
+    }
+
+    function salvar(Request $req){
+        $id = $req->input('id');
+        $nome = $req->input('nome');
+        $estado = $req->input('estado');
+
+        $cidade = Cidade::findOrFail($id);
+        $cidade->nome = $nome;
+        $cidade->estado = $estado;
+
+        $cidade->save();
+
+        return redirect()->route('cidades_listar');
+    }
+
+    function excluir($id){
+        $cidade = Cidade::findOrFail($id);
+        $cidade->delete();
+
+        return redirect()->route('cidades_listar');
+    }
+}
--- database/migrations/2022_05_03_005737_create_credito.php
+++ database/migrations/2022_05_03_005737_create_credito.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('credito', function (Blueprint $table) {
             $table->id();
-            $table->double('saldo', 20, 2);
+            $table->double('valor', 20, 2);
+            $table->date('data');
             $table->unsignedBigInteger('id_cliente');
 
             $table->foreign('id_cliente')->references('id')->on('clientes');
