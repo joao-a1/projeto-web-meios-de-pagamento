bf10dd26d87d2cc9d4fdb1839967b509d69ba58a
Gabriel Correa <62802360+GabrielCDias@users.noreply.github.com>
2022-05-16 13:01:39 -0300

    Middleware

--- routes/web.php
+++ routes/web.php
@@ -34,46 +34,43 @@ Route::get('/dashboard', function () {
         Route::get('/clientes/excluir/{id}', [ClientesController::class, 'excluir'])->name('clientes_excluir');
         Route::get('/clientes/listar', [ClientesController::class, 'listar'])->name('clientes_listar');
 
-        Route::get('/empresas/novo', [EmpresasController::class, 'cadastro_novo']);  
-        Route::post('/empresas/novo', [EmpresasController::class, 'novo'])->name('empresas_novo');
-        Route::get('/empresas/alterar/{id}', [EmpresasController::class, 'alterar'])->name('empresas_alterar');
-        Route::post('/empresas/alterar/', [EmpresasController::class, 'salvar'])->name('empresas_salvar');
-        Route::get('/empresas/excluir/{id}', [EmpresasController::class, 'excluir'])->name('empresas_excluir');
-        Route::get('/empresas/listar', [EmpresasController::class, 'listar'])->name('empresas_listar');
-
-        Route::get('/transacoes/novo', [TransacoesController::class, 'cadastro_novo']);
-        Route::post('/transacoes/novo', [TransacoesController::class, 'novo'])->name('transacoes_novo');
-        Route::get('/transacoes/alterar/{id}', [TransacoesController::class, 'alterar'])->name('transacoes_alterar');
-        Route::post('/transacoes/alterar/', [TransacoesController::class, 'salvar'])->name('transacoes_salvar');
-        Route::get('/transacoes/excluir/{id}', [TransacoesController::class, 'excluir'])->name('transacoes_excluir');
-        Route::get('/transacoes/listar', [TransacoesController::class, 'listar'])->name('transacoes_listar');
-
-
-        Route::get('/creditos/novo', [CreditosController::class, 'cadastro_novo']);
-        Route::post('/creditos/novo', [CreditosController::class, 'novo'])->name('creditos_novo');
-        Route::get('/creditos/alterar/{id}', [CreditosController::class, 'alterar'])->name('creditos_alterar');
-        Route::post('/creditos/alterar/', [CreditosController::class, 'salvar'])->name('creditos_salvar');
-        Route::get('/creditos/excluir/{id}', [CreditosController::class, 'excluir'])->name('creditos_excluir');
-        Route::get('/creditos/listar', [CreditosController::class, 'listar'])->name('creditos_listar');
-
-        Route::get('/cidades/novo', [CidadesController::class, 'cadastro_novo']);
-        Route::post('/cidades/novo', [CidadesController::class, 'novo'])->name('cidades_novo');
-        Route::get('/cidades/alterar/{id}', [CidadesController::class, 'alterar'])->name('cidades_alterar');
-        Route::post('/cidades/alterar/', [CidadesController::class, 'salvar'])->name('cidades_salvar');
-        Route::get('/cidades/excluir/{id}', [CidadesController::class, 'excluir'])->name('cidades_excluir');
-        Route::get('/cidades/listar', [CidadesController::class, 'listar'])->name('cidades_listar');
-
-        Route::get('/status/novo', [StatusController::class, 'cadastro_novo']);
-        Route::post('/status/novo', [StatusController::class, 'novo'])->name('status_novo');
-        Route::get('/status/alterar/{id}', [StatusController::class, 'alterar'])->name('status_alterar');
-        Route::post('/status/alterar/', [StatusController::class, 'salvar'])->name('status_salvar');
-        Route::get('/status/excluir/{id}', [StatusController::class, 'excluir'])->name('status_excluir');
-        Route::get('/status/listar', [StatusController::class, 'listar'])->name('status_listar');
-
-
     Route::middleware('auth')->group(function () {
         Route::middleware('verifica.nivel')->group(function(){
-        
+            Route::get('/empresas/novo', [EmpresasController::class, 'cadastro_novo']);  
+            Route::post('/empresas/novo', [EmpresasController::class, 'novo'])->name('empresas_novo');
+            Route::get('/empresas/alterar/{id}', [EmpresasController::class, 'alterar'])->name('empresas_alterar');
+            Route::post('/empresas/alterar/', [EmpresasController::class, 'salvar'])->name('empresas_salvar');
+            Route::get('/empresas/excluir/{id}', [EmpresasController::class, 'excluir'])->name('empresas_excluir');
+            Route::get('/empresas/listar', [EmpresasController::class, 'listar'])->name('empresas_listar');
+    
+            Route::get('/transacoes/novo', [TransacoesController::class, 'cadastro_novo']);
+            Route::post('/transacoes/novo', [TransacoesController::class, 'novo'])->name('transacoes_novo');
+            Route::get('/transacoes/alterar/{id}', [TransacoesController::class, 'alterar'])->name('transacoes_alterar');
+            Route::post('/transacoes/alterar/', [TransacoesController::class, 'salvar'])->name('transacoes_salvar');
+            Route::get('/transacoes/excluir/{id}', [TransacoesController::class, 'excluir'])->name('transacoes_excluir');
+            Route::get('/transacoes/listar', [TransacoesController::class, 'listar'])->name('transacoes_listar');
+    
+    
+            Route::get('/creditos/novo', [CreditosController::class, 'cadastro_novo']);
+            Route::post('/creditos/novo', [CreditosController::class, 'novo'])->name('creditos_novo');
+            Route::get('/creditos/alterar/{id}', [CreditosController::class, 'alterar'])->name('creditos_alterar');
+            Route::post('/creditos/alterar/', [CreditosController::class, 'salvar'])->name('creditos_salvar');
+            Route::get('/creditos/excluir/{id}', [CreditosController::class, 'excluir'])->name('creditos_excluir');
+            Route::get('/creditos/listar', [CreditosController::class, 'listar'])->name('creditos_listar');
+    
+            Route::get('/cidades/novo', [CidadesController::class, 'cadastro_novo']);
+            Route::post('/cidades/novo', [CidadesController::class, 'novo'])->name('cidades_novo');
+            Route::get('/cidades/alterar/{id}', [CidadesController::class, 'alterar'])->name('cidades_alterar');
+            Route::post('/cidades/alterar/', [CidadesController::class, 'salvar'])->name('cidades_salvar');
+            Route::get('/cidades/excluir/{id}', [CidadesController::class, 'excluir'])->name('cidades_excluir');
+            Route::get('/cidades/listar', [CidadesController::class, 'listar'])->name('cidades_listar');
+    
+            Route::get('/status/novo', [StatusController::class, 'cadastro_novo']);
+            Route::post('/status/novo', [StatusController::class, 'novo'])->name('status_novo');
+            Route::get('/status/alterar/{id}', [StatusController::class, 'alterar'])->name('status_alterar');
+            Route::post('/status/alterar/', [StatusController::class, 'salvar'])->name('status_salvar');
+            Route::get('/status/excluir/{id}', [StatusController::class, 'excluir'])->name('status_excluir');
+            Route::get('/status/listar', [StatusController::class, 'listar'])->name('status_listar');
     });
 });
 
